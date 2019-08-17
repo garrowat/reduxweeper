689def4511802db04d4de521097b1430a078dac9
Garrett Watson <garrettbauer@gmail.com>
2019-08-17 03:21:57 +0000

    Redo App.js to conditionally render splash header or game board, add start game button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import logo from './logo512.png';
+import logo from './logo600.png';
 import './App.css';
+import GameBoard from './components/GameBoard.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} />
-        <h1>
-          Reduxweeper
-        </h1>
-        <p>
-          Minesweeper in React/Redux
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      game: {
+        isEnded: false,
+        isStarted: false,
+        isWon: false,
+      },
+    }
+    this.startGame = this.startGame.bind(this);
+  }
+
+  startGame() {
+    this.setState({ isStarted: true })
+  }
+
+  render() {
+    const { isStarted } = this.state;
+    return (
+      <div className="App">
+        {
+          isStarted
+           ? <GameBoard />
+           : (
+            <header className="App-header">
+              <img alt="Reduxweeper" src={logo} />
+              <h1>
+                Reduxweeper
+              </h1>
+              <button className='blue' onClick={this.startGame} >Start</button>
+              <p>
+                Minesweeper in React/Redux
+              </p>
+            </header>
+           )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
