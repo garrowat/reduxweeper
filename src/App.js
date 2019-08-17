5894776711bd8768f8f7f30590c02d70ba0e8479
Garrett Watson <garrettbauer@gmail.com>
2019-08-17 00:53:09 +0000

    Set up initial repo, add logo

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo512.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} />
+        <h1>
+          Reduxweeper
+        </h1>
+        <p>
+          Minesweeper in React/Redux
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
